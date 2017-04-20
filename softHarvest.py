ff951bcbb222a2c6ef1adb3f8bdf93e55b759595
Kat Thornton <katherine.thornton@yale.edu>
2017-04-20 12:49:04 -0400

    Create softHarvest.py

---
+++ softHarvest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 
+#   FILE: softHarvest.py
+#   DATE: April 20, 2017
+#   Author: Katherine Thornton
+#
+
+#This is a program written in Python3 that will start from a list of Yale
+#University Library (YUL) barcodes and then pull out some fields from the 
+#JSON returned by the Voyager API to feed into YUL bot for Wikidata uploading. 
+
+import requests
+
+baseurl = "http://deleon.library.yale.edu:9090/VoySearch/GetMfhdItem?barcode="
+url = ""
+barcodes = ["39002076606962", "39002046083961", "39002067185075"]
+for barcode in barcodes:
+    baseurl+= barcode
+    print(url)
